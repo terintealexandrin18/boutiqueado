05fd6da44075132fc6cf4c3ddd72c06f8ae09508
Alex Terinte <terintealexandrin18@gmail.com>
2024-04-24 19:41:52 +0000

    Add quantity input + and - buttons

--- bag/contexts.py
+++ bag/contexts.py
@@ -22,16 +22,15 @@ def bag_contents(request):
             })
         else:
             product = get_object_or_404(Product, pk=item_id)
-            for size, quantity in item_data['item_by_size'].items():
+            for size, quantity in item_data['items_by_size'].items():
                 total += quantity * product.price
                 product_count += quantity
                 bag_items.append({
                     'item_id': item_id,
                     'quantity': item_data,
                     'product': product,
-                    'size' : size,
+                    'size': size,
                 })
-
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
